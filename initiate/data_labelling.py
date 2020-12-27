972b80db6e8517b26368ddad5072280f041ebf8f
EunWoo Im <stevena9@naver.com>
2020-12-28 03:56:19 +0900

    Create data_labelling.py

---
+++ initiate/data_labelling.py
@@ -0,0 +1,18 @@
+#Labelling
+import os
+from functions import walk, loaddm3, matnorm255, matchhist, imshow
+
+f = open('classifier.csv','w')
+count=0
+for folder, filename in walk(destdir):
+  ext = os.path.splitext(filename)[-1]
+  if not isdm3(ext):
+    continue
+  filepath = os.path.join(folder, filename).replace("\\","/")
+  x = loaddm3(filepath)
+  img = matnorm255(x)
+  img = matchhist(img,ref)
+  imshow(img)
+  i = input()
+  f.write(filepath + ',' + str(i) + '\n')
+f.close()
