596f0d98090eb2056e62fec2acb69610ff0c3090
EunWoo Im <stevena9@naver.com>
2020-12-28 14:29:19 +0900

    Update functions.py

--- initiate/functions.py
+++ initiate/functions.py
@@ -52,28 +52,6 @@ def loaddm3(name):
             newname = name+'.dm4'
             matrix = hs.load(newname).data
     return matrix
-    
-def rotate_images(X_imgs, start_angle, end_angle, n_images):
-    '''Rotate image with input angle'''
-    X_rotate = []
-    iterate_at = (end_angle - start_angle) / (n_images - 1)
-    from math import pi
-    tf.reset_default_graph()
-    X = tf.placeholder(tf.float32, shape = (None, IMAGE_SIZE, IMAGE_SIZE, 3))
-    radian = tf.placeholder(tf.float32, shape = (len(X_imgs)))
-    tf_img = tf.contrib.image.rotate(X, radian)
-    with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
-    
-        for index in range(n_images):
-            degrees_angle = start_angle + index * iterate_at
-            radian_value = degrees_angle * pi / 180  # Convert to radian
-            radian_arr = [radian_value] * len(X_imgs)
-            rotated_imgs = sess.run(tf_img, feed_dict = {X: X_imgs, radian: radian_arr})
-            X_rotate.extend(rotated_imgs)
-
-    X_rotate = np.array(X_rotate, dtype = np.float32)
-    return X_rotate
   
 def plot_confusion_matrix(data, labels, output_filename):
     """Plot confusion matrix using heatmap.
