467d0c178833c23689c0ded5f6eb2538fba148b5
EunWoo Im <stevena9@naver.com>
2020-12-28 14:29:45 +0900

    Update computervision.py

--- RuleBased/computervision.py
+++ RuleBased/computervision.py
@@ -182,3 +182,25 @@ def fit_line_ransac(data,iter=30,sample_num=10,offset=80.0):
 			count_max = len(ran_sample) 
 			      effective_sample = ran_sample
 	return fit_rotated_ellipse(effective_sample)
+    
+def rotate_images(X_imgs, start_angle, end_angle, n_images):
+    '''Rotate image with input angle'''
+    X_rotate = []
+    iterate_at = (end_angle - start_angle) / (n_images - 1)
+    from math import pi
+    tf.reset_default_graph()
+    X = tf.placeholder(tf.float32, shape = (None, IMAGE_SIZE, IMAGE_SIZE, 3))
+    radian = tf.placeholder(tf.float32, shape = (len(X_imgs)))
+    tf_img = tf.contrib.image.rotate(X, radian)
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+    
+        for index in range(n_images):
+            degrees_angle = start_angle + index * iterate_at
+            radian_value = degrees_angle * pi / 180  # Convert to radian
+            radian_arr = [radian_value] * len(X_imgs)
+            rotated_imgs = sess.run(tf_img, feed_dict = {X: X_imgs, radian: radian_arr})
+            X_rotate.extend(rotated_imgs)
+
+    X_rotate = np.array(X_rotate, dtype = np.float32)
+    return X_rot
